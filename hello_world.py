f7a68208469941941f5365cd1adb4ca0e5c7323c
= <setient@gmail.com>
2015-06-12 09:24:02 -0700

    removing hello_world.py since it really was not a hello world

--- hello_world.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/python
-import argparse
-import boto
-parser = argparse.ArgumentParser()
-parser.add_argument("hostname", help="hostname of the vm you wish to create")
-parser.parse_args()
-print args.hostname
